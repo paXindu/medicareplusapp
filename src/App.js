f4843a7e38fac0d62db7c962810c16b6180f4626
Pasindu Bathiya Bandara <62470295+paXindu@users.noreply.github.com>
2023-12-06 09:12:20 +0530

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center min-h-screen bg-cover bg-center bg-gray-200">
+      <div className="bg-gray-800 p-8 rounded-lg shadow-md w-96 h-auto text-center text-white">
+        <h1 className="text-2xl font-semibold mb-4">Welcome to MediCarePlus</h1>
+        <p className="text-gray-300 mb-6">Comprehensive Healthcare Management System with Heart Disease Prediction System.</p>
+        <div className="flex flex-col space-y-3">
+          <a
+            href="/patientlogin"
+            className="w-full py-2 px-4 border rounded-md text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-800"
+          >
+            Patient Login
+          </a>
+          <a
+            href="/patientregistration"
+            className="w-full py-2 px-4 border rounded-md text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-800"
+          >
+            Patient Self Registration
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
